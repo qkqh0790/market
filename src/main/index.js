6cc7887c1cbbf9b339abc02023dacfd9cc80c119
github qkqh0790 <qkqh0790@naver.com>
2021-05-12 21:01:51 +0900

    project init

---
+++ src/main/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./index.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://bdf69477-81bf-4e22-8b4a-b6bd7a692a6c.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="banner">
+        <img src="/images/banners/banner1.png" />
+      </div>
+      <h1>오늘은 이거 어때?</h1>
+      <div id="product-list">
+        {products.map(function (product, index) {
+          return (
+            <div className="product-card">
+              <Link
+                style={{ color: "inherit" }}
+                className="product-link"
+                to={`/products/${product.id}`}
+              >
+                <div>
+                  <img className="product-img" src={product.imageUrl} />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+export default MainPage;
